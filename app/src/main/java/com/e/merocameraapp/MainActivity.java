553557d6dfcca2e07686e202933bf36377a66d34
Softwarica College Lab <softwarica@softwarica.com>
2019-04-26 07:55:49 +0545

    Check permission is defined

--- app/src/main/java/com/e/merocameraapp/MainActivity.java
+++ app/src/main/java/com/e/merocameraapp/MainActivity.java
@@ -1,13 +1,34 @@
 package com.e.merocameraapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageview;
+    private Button btnCapture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermission();
+        imageview=findViewById(R.id.imageview);
+        btnCapture=findViewById(R.id.btnCapture);
     }
+
+    private void checkPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE},0);
+        }
+    }
+
 }
